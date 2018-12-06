756db63152f3495a2aba717119e680607f0c9705
SPAceRROW <spacerrow@gmail.com>
2018-12-06 16:59:19 +0200

    add what-need-screen + adaptive

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
 import $ from 'jquery';
 
+
 $(window).scroll(function() {
   if($(this).scrollTop() > 70) {
     $('.header').addClass('scroll');
@@ -10,6 +11,18 @@ $(window).scroll(function() {
 });
 
 $(document).ready(function() {
+  // - - - - NUMBER - BEFORE - - - -
+  var num = Number($('.what-need__block').length), i, str, elem;
+  for (i = 1; i <= num; i++) {
+    str = i;
+    if (str < 10) {
+      elem = '0'+i;
+    } else {
+      elem = i;
+    }
+    document.styleSheets[0].addRule('.what-need__block:nth-of-type('+i+')::before','content: "'+elem+'";');
+  }
+  // - - - - HAMBURGER - - - -
   $('.nav__button').click(function() {
     $('.header').toggleClass('active');
     $('.nav__button').toggleClass('active');
